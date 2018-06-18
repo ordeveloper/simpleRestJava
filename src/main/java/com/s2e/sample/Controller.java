d059fcfeb29a89386efec3cfdb086bba3a972807
Matteo Pisani <matteo.pisani@s2e-pro.com>
2018-06-18 12:59:46 +0200

    methods

--- src/main/java/com/s2e/sample/Controller.java
+++ src/main/java/com/s2e/sample/Controller.java
@@ -1,10 +1,12 @@
 package com.s2e.sample;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,27 +28,50 @@ public class Controller {
 	@RequestMapping(value = "/cpu", method = RequestMethod.GET)
 	@ResponseBody
 	// restituisce la lista dei numeri primi fino a x milioni
-	public void calcolaNumeriPrimi() {
-
+	public void calcolaNumeriPrimi(@RequestParam(name="n", required=false, defaultValue="1000000") long n) {
+		ArrayList<Integer> setPrimi = new ArrayList<Integer>();
+		for (int i = 2; i <= n; i++) {
+			setPrimi.add(i);
+		}
+		
+		for (Integer i : setPrimi) {
+			for (int k = 2*i; k <= n; k += i) {
+				setPrimi.remove(new Integer(k));
+			}
+		}
 		log.info("test lettura cpu");
 	}
 
 	@RequestMapping(value = "/io", method = RequestMethod.GET)
 	@ResponseBody
 	// restituisce un grosso messaggio a partire da quello di input
-	public String ritornaMexIO(String message) {
-
+	public String ritornaMexIO(@RequestParam(name="mex", required=false, defaultValue="Hello Test World") String message) {
+		String newMessage = message  + " AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+				+ "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+				+ "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC";
+		
+		
 		log.info("Test trasmissione i/o");
+		return newMessage;
 
-		return null;
 	}
 
 	@RequestMapping(value = "/ram", method = RequestMethod.GET)
 	@ResponseBody
 	// restituisce un grosso vettore pieno e poi vuoto
-	public String ritornaVettore() {
+	public void ritornaVettore(@RequestParam(name="n", required=false, defaultValue="1000") long n) {
+		
+		ArrayList<Character> vettore = new ArrayList<Character>();
+		
+		for (int i = 0; i < n ; i++) {
+			vettore.add('a');
+		}
+		
+		for (Character i : vettore) {
+			vettore.remove(i);
+		}
+		
 		log.info("Test lettura ram");
-		return null;
 	}
 
 	@RequestMapping(value = "/error", method = RequestMethod.GET)
