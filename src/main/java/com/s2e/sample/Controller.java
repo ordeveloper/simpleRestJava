a5c930c74c9051d7b5921fb51aa3cfc9d9ce0212
AzureAD\RobertoOggionni <roberto.oggionni@s2e-mail.com>
2018-06-18 11:55:56 +0200

    bozza impelementazione controlli

--- src/main/java/com/s2e/sample/Controller.java
+++ src/main/java/com/s2e/sample/Controller.java
@@ -1,6 +1,7 @@
 package com.s2e.sample;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,6 +21,40 @@ public class Controller {
       return a;
     }
 	
+	@RequestMapping(value = "/cpu", method = RequestMethod.GET)
+	@ResponseBody
+    public void calcolaNumeriPrimi() {
+       // restituisce la lista dei numeri primi fino a x milioni
+    }
 	
+	@RequestMapping(value = "/io", method = RequestMethod.GET)
+	@ResponseBody
+    public String ritornaMexIO(String message) {
+       // restituisce un grosso messaggio a partire da quello di input
+		return null;
+    }
+	
+	@RequestMapping(value = "/ram", method = RequestMethod.GET)
+	@ResponseBody
+    public String ritornaVettore() {
+       // restituisce un grosso vettore pieno e poi vuoto
+		return null;
+    }
+	
+	
+	@RequestMapping(value = "/error", method = RequestMethod.GET)
+	@ResponseBody
+    public HttpStatus generaErrore() {
+       
+		// il 5% dei casi ritorna errore altrimento OK
+		Double prob = Math.random();
+		if (prob < 0.05) {
+			return HttpStatus.INTERNAL_SERVER_ERROR;
+		}else {
+			return HttpStatus.OK;
+		}
+		
+		
+    }
 	
 }
